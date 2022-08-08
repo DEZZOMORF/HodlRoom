efe7e1428c3043cb664443b59b2f2a0f813bc1f3
Pavel Netrebchuk <pashokukr@gmail.com>
2022-08-08 12:24:45 +0300

    Added item touch helper

---
+++ app/src/main/java/com/lampa/financulator/extensions/StaticLayoutExtensions.kt
@@ -0,0 +1,11 @@
+package com.lampa.financulator.extensions
+
+import android.graphics.Canvas
+import android.text.StaticLayout
+import androidx.core.graphics.withTranslation
+
+fun StaticLayout.draw(canvas: Canvas, x: Float, y:Float) {
+    canvas.withTranslation(x,y) {
+        draw(this)
+    }
+}
